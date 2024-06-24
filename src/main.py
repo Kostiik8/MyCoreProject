000f332559cacd2a6ae5c1279c0f4c2c180cf0c6
Artyukhin Konstantin <k89161273297@mail.ru>
2024-06-24 16:02:23 +0300

    homework_14_1 done

--- src/main.py
+++ src/main.py
@@ -1,18 +1,28 @@
 class Category:
     """Класс для представления категорий"""
-    total_amount_category = 0
-    unique_products = set()
 
-    def __init__(self, name: str, description: str, products: str):
+    unique_products = 0
+    total_amount_category = 0
+
+    def __init__(self, name: str, description: str, products: list):
         self.name = name
         self.description = description
-        self.products = products
+        self.products = set(products)
+        Category.total_amount_category += 1
+        Category.unique_products = len(set(self.products))
 
 
 class Product:
     """Класс для представления продукта"""
+
     def __init__(self, name: str, description: str, price: float, amount: float):
         self.name = name
         self.description = description
         self.price = price
         self.amount = amount
+
+
+cat1 = Category("Фрукты", "Различные свежие фрукты", ["Банан", "Груша", "Груша"])
+cat2 = Category("Овощи", "Различные  Овощи", ["Морковь", "Морковь", "Свекла"])
+print(Category.total_amount_category)
+print(Category.unique_products)
---
+++ tests/test_main.py
@@ -0,0 +1,28 @@
+import pytest
+
+from src.main import Category, Product
+
+
+@pytest.fixture
+def test_category():
+    return Category("Фрукты", "Различные свежие фрукты", ["Банан", "Груша", "Груша"])
+
+
+def test_init(test_category):
+    assert test_category.name == "Фрукты"
+    assert test_category.description == "Различные свежие фрукты"
+    assert test_category.products == {"Банан", "Груша"}
+    assert test_category.total_amount_category == 3
+    assert test_category.unique_products == 2
+
+
+@pytest.fixture
+def test_product():
+    return Product("Яблоко", "Фрукт", 100.1, 10)
+
+
+def test_init_2(test_product):
+    assert test_product.name == "Яблоко"
+    assert test_product.description == "Фрукт"
+    assert test_product.price == 100.1
+    assert test_product.amount == 10
