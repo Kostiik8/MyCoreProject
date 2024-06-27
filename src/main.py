01d7f616203a06533cda632818f797ef66e6cc3b
Artyukhin Konstantin <k89161273297@mail.ru>
2024-06-27 23:27:46 +0300

    finish homework 14_2

--- src/main.py
+++ src/main.py
@@ -3,19 +3,82 @@ class Category:
 
     total_amount_category = 0
 
-    def __init__(self, name: str, description: str, products: list):
+    def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = set(products)
-        self.unique_products = len(self.products)
+        self.__products = set(products)
+        self.unique_products = len(self.__products)
         Category.total_amount_category += 1
+
+    def add_product(self, products):
+        """Добавление продукта в категорию."""
+        self.__products.add(products)
+        self.unique_products = len(self.__products)
+
+    @property
+    def products(self):
+        """Геттер для вывода списка товаров в заданном формате"""
+        product_list = []
+        for product in self.__products:
+            product_list.append(f"{product.name}, {product.price} руб. Остаток: {product.amount} шт.")
+        return product_list
 
 
 class Product:
     """Класс для представления продукта"""
 
-    def __init__(self, name: str, description: str, price: float, amount: float):
-        self.name = name
+    def __init__(self, name: str, description, price: float, amount: float):
+        self._name = name
         self.description = description
-        self.price = price
-        self.amount = amount
+        self._price = price
+        self._amount = amount
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if value <= 0:
+            print("Некорректное цена")
+        else:
+            self._price = value
+
+    @price.deleter
+    def price(self):
+        del self._price
+
+    @property
+    def amount(self):
+        return self._amount
+
+    @amount.setter
+    def amount(self, value):
+        if value <= 0:
+            print("Некорректное количество")
+        else:
+            self._amount = value
+
+    @amount.deleter
+    def amount(self):
+        del self._amount
+
+    @classmethod
+    def create_product(cls, name, description, price, amount):
+        """Метод, который создает товар и возвращает объект"""
+        return cls(name, description, price, amount)
+
+
+product1 = Product("Банан", "фрукт", 15, 6)
+product2 = Product("Груша", "фрукты", 10, 5)
+product3 = Product("Яблоко", "фрукт", 20, 1)
+
+category = Category("Фрукты", "Различные свежие фрукты", [product1, product2, product2])
+print(category.products)
+
+category.add_product(product3)
+print(category.products)
--- tests/test_main.py
+++ tests/test_main.py
@@ -5,26 +5,53 @@ from src.main import Category, Product
 
 @pytest.fixture
 def test_category():
-    category1 = Category("Фрукты", "Различные свежие фрукты", ["Банан", "Груша", "Груша"])
-    category2 = Category("Овощи", "Различные свежие овощи", ["Морковь", "Капуста"])
-    return category1
+    product1 = Product("Банан", "фрукт", 15, 6)
+    product2 = Product("Груша", "фрукты", 10, 5)
+    category = Category("Фрукты", "Различные свежие фрукты", [product1, product2, product2])
+    return category
 
 
 def test_init(test_category):
     assert test_category.name == "Фрукты"
     assert test_category.description == "Различные свежие фрукты"
-    assert test_category.products == {"Банан", "Груша"}
+    assert set(test_category.products) == {"Банан, 15 руб. Остаток: 6 шт.", "Груша, 10 руб. Остаток: 5 шт."}
+
+
+def test_add_product(test_category):
+    new_product = Product("Яблоко", "Фрукт", 50, 20)
+    test_category.add_product(new_product)
+    assert set(test_category.products) == {
+        "Банан, 15 руб. Остаток: 6 шт.",
+        "Груша, 10 руб. Остаток: 5 шт.",
+        "Яблоко, 50 руб. Остаток: 20 шт.",
+    }
+
+
+def test_set_price():
+    product = Product("Яблоко", "фрукт", 0, 10)
+    product.price = 20
+    assert product.price == 20
+
+
+def test_set_amount():
+    product = Product("Яблоко", "фрукт", 20, 10)
+    product.amount = 15
+    assert product.amount == 15
+
+
+def test_class(test_category):
     assert test_category.unique_products == 2
-    assert Category.total_amount_category == 2
+    assert Category.total_amount_category == 4
 
 
 @pytest.fixture
 def test_product():
-    return Product("Яблоко", "Фрукт", 100.1, 10)
+    product1 = Product("Банан", "Фрукт", 15, 6)
+    return product1
 
 
 def test_init_2(test_product):
-    assert test_product.name == "Яблоко"
+    assert test_product.name == "Банан"
     assert test_product.description == "Фрукт"
-    assert test_product.price == 100.1
-    assert test_product.amount == 10
+    assert test_product.price == 15
+    assert test_product.amount == 6
